055c01e19b2328ff1c22012409a4aa686defe299
slava <ssenchenko@gmail.com>
2019-06-27 17:37:03 -0400

    toTitle function, since no standard in JS

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,4 +1,11 @@
-const toTitle = (str?: string | null): string | null | undefined =>
-  str && str[0].toUpperCase() + str.slice(1);
+/**
+ * Make string a title by upper-casing the first symbol.
+ *
+ * @param str - string, null, or undefined
+ * @returns string, null, or undefined
+ */
+function toTitle(str?: string | null): string | null | undefined {
+  return str && str[0].toUpperCase() + str.slice(1);
+}
 
 export default toTitle;
