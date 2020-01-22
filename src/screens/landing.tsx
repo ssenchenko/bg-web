0c5c13bcad8b6a4637f1019e2a33eefb7c436fe0
Slava Senchenko <ssenchenko@gmail.com>
2020-01-21 22:54:13 -0500

    eslint and children

--- src/screens/landing.tsx
+++ src/screens/landing.tsx
@@ -2,28 +2,34 @@ import React from "react";
 import { Container, Typography, Box } from "@material-ui/core";
 import { styled, useTheme } from "@material-ui/core/styles";
 
-class PageLayout extends React.Component<{}, {}> {
-  render() {
-    const theme = useTheme();
-    const layout = styled(Box)({
-      display: "flex",
-      flexDirection: "column-reverse",
-      [theme.breakpoints.up("md")]: {
-        flexDirection: "column",
-      },
-    });
-    return layout;
-  }
-}
+type PageLayoutProps = {
+  children: React.ReactNode;
+};
 
-const LandingScreen: React.FC = () => (
-  <>
-    <Container maxWidth="lg">
-      <PageLayout>
-        <Typography>Hello</Typography>
-      </PageLayout>
-    </Container>
-  </>
-);
+const PageLayout: React.FC<PageLayoutProps> = ({ children }: PageLayoutProps) => {
+  const theme = useTheme();
+
+  const StyledBox = styled(Box)({
+    display: "flex",
+    flexDirection: "column-reverse",
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "column",
+    },
+  });
+
+  return <StyledBox>{children}</StyledBox>;
+};
+
+const LandingScreen: React.FC = () => {
+  return (
+    <>
+      <Container maxWidth="lg">
+        <PageLayout>
+          <Typography>Hello</Typography>
+        </PageLayout>
+      </Container>
+    </>
+  );
+};
 
 export default LandingScreen;
